34072569d8692b1e83e945ea1cf5c091c36e8726
dit08 <dit@imac-3.local>
2019-04-25 17:14:14 +0900

    아이폰앱개발 중간고사 문제 04번

--- 04/ViewController.swift
+++ 04/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func btn(_ sender: Any) {
+        let AlertController = UIAlertController(title: "Hello", message: "Alert Test", preferredStyle: .alert)
+        
+        let cam = UIAlertAction(title: "사진 촬영", style: .default, handler: {
+            (action: UIAlertAction) -> Void in
+            self.lbl.text = "사진을 촬영합니다."
+        })
+        
+        let cancel = UIAlertAction(title: "취소", style: .cancel) {
+            (action: UIAlertAction) -> Void in
+            self.lbl.text = "취소 했습니다."
+        }
+        
+        AlertController.addAction(cam)
+        AlertController.addAction(cancel)
+        
+        self.present(AlertController, animated: true, completion: nil)
+    }
+    
 
 }
 
